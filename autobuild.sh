1ca0eee6221f3457b4fe68387c869ee5f70c862d
Tom Cobb <tom.cobb@diamond.ac.uk>
2022-10-06 11:39:39 +0100

    Fix build

--- autobuild.sh
+++ autobuild.sh
@@ -1,5 +1,6 @@
-if [ ! -e venv ]; then
-    python3.9 -m venv venv
-    venv/bin/python -m pip install -r requirements.txt
+VENV=/tmp/developer-portal-venv
+if [ ! -e $VENV ]; then
+    python3.9 -m venv $VENV
+    $VENV/bin/python -m pip install -r requirements.txt
 fi
-venv/bin/sphinx-autobuild -WT docs build/html
+$VENV/bin/sphinx-autobuild -WT docs build/html
